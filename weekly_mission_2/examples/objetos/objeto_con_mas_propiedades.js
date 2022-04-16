86191d98b4649e8a0032e07d32905e01d5d968c5
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-04-16 16:23:41 -0500

    Se restructuro el acomodo de los ejemplos de los objetos

---
+++ weekly_mission_2/examples/objetos/objeto_con_mas_propiedades.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Raul",
+    age: 22,
+    nicknames: [
+     "RikuSam4",
+      "xXcHaVe2Xx",
+      "Chavez"
+    ],
+    address: {
+      zip_code: "28548",
+      street: "Dr. Vertiz 11 Benito Juarez",
+      city: "Colima"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
